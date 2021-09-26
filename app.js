163d9f148db5f47d553addba0b0c7e1da5ddbac5
DollyChen_CYC <dolly726033@gmail.com>
2021-09-26 19:40:42 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,13 +3,25 @@
 // step 3: npm i express express-handlebars
 // step 4: revise package.json => add   "scripts": {  "start": "node app.js", "dev": "nodemon app.js", ...
 // step 5: start writing server related code to init the project
+// step 6: create new DB by Robo3T => install Mongoose by "npm i mongoose" => setting connection in app.js
 
 // include packages and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// connect to mongo DB
+mongoose.connect('mongodb://localhost/todo-list')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
 
 // setting template engine
 app.engine('hbs', exphbs ({ defaultLayout: 'main', extname: '.hbs'}))
