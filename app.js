e501edb281e37bbd7d478fb4c1fcabacc2345913
DollyChen_CYC <dolly726033@gmail.com>
2022-01-17 16:10:46 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -14,6 +15,13 @@ app.engine('hbs', exphbs ({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 // use method-override
 app.use(methodOverride('_method'))
 // setting body-parser
