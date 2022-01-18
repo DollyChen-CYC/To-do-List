9514fd9b95a4485e30c1ac15046aadea8cd5af19
DollyChen_CYC <dolly726033@gmail.com>
2022-01-18 21:02:43 +0800

    feat: modify navbar and home view to fit authentication state

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use(express.urlencoded({ extended: true }))
 
 // use passport.js
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated
+  res.locals.user = req.user
+  next()
+})
 
 // setting routes
 app.use(routes)
